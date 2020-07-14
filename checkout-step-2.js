8997432a3d71bcaa5cce59c10f78186b8973c9eb
fnovais1987 <68257765+fnovais1987@users.noreply.github.com>
2020-07-13 22:30:45 -0300

    Create checkout-step-2.js

---
+++ checkout-step-2.js
@@ -0,0 +1,32 @@
+<script>
+/**
+ * data layer variable to handle navigation after the ecommerce data has been sent to Google Analytics on /checkout.
+ */
+
+  dataLayer.push({
+    'event': 'checkout',
+    'ecommerce': {
+      'checkout': {
+        'actionField': {'step': 2},
+        'products': [{
+          'name': 'COMBO 4 POTES DE SPORT PROTEIN MORANGO (50% OFF + 5% OFF no boleto)',         // Name or ID is required.
+          'id': '"8756322256745"',
+          'price': '756.00',
+          'brand': 'Mother',
+          'category': 'Protein',
+          'variant': '',
+          'quantity': 1 
+       },{
+          'name': 'Combo 12 Chocolates Proteicos - 40g',         // if there are more products on cart inser an array position
+          'id': '"5823645123658"',
+          'price': '156.00',
+          'brand': 'Mother',
+          'category': 'Protein',
+          'variant': '',
+          'quantity': 1 
+       }]
+     }
+   },
+  });
+
+</script>

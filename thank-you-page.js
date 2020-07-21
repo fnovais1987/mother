7e10e1b034b2359a97308a0e0d76c449d4a3e5ff
Novais <68257765+fnovais1987@users.noreply.github.com>
2020-07-20 21:41:05 -0300

    Update thank-you-page.js

--- thank-you-page.js
+++ thank-you-page.js
@@ -16,7 +16,7 @@ dataLayer.push({
       },
       'products': [{
           'name': 'COMBO 4 POTES DE SPORT PROTEIN MORANGO (50% OFF + 5% OFF no boleto)',         // Name or ID is required.
-          'id': '"8756322256745"',
+          'id': '8756322256745',
           'price': '756.00',
           'brand': 'Mother',
           'category': 'Protein',
@@ -24,7 +24,7 @@ dataLayer.push({
           'quantity': 1 
        },{
           'name': 'Combo 12 Chocolates Proteicos - 40g',         // if there are more products on cart inser an array position
-          'id': '"5823645123658"',
+          'id': '5823645123658',
           'price': '156.00',
           'brand': 'Mother',
           'category': 'Protein',

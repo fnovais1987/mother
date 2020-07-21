72ac905facaabaf6d9e43676b1bec0f85cf07461
Novais <68257765+fnovais1987@users.noreply.github.com>
2020-07-20 21:37:36 -0300

    Update checkout-step-1-cart.js

--- checkout-step-1-cart.js
+++ checkout-step-1-cart.js
@@ -10,7 +10,7 @@
         'actionField': {'step': 1},
         'products': [{
           'name': 'COMBO 4 POTES DE SPORT PROTEIN MORANGO (50% OFF + 5% OFF no boleto)',         // Name or ID is required.
-          'id': '"8756322256745"',
+          'id': '8756322256745',
           'price': '756.00',
           'brand': 'Mother',
           'category': 'Protein',
@@ -18,7 +18,7 @@
           'quantity': 1 
        },{
           'name': 'Combo 12 Chocolates Proteicos - 40g',         // if there are more products on cart inser an array position
-          'id': '"5823645123658"',
+          'id': '5823645123658',
           'price': '156.00',
           'brand': 'Mother',
           'category': 'Protein',

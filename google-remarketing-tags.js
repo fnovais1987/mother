aa4de5711b5c789d9914f718239b3b7be95098d9
Novais <68257765+fnovais1987@users.noreply.github.com>
2020-07-20 21:39:08 -0300

    Update google-remarketing-tags.js

--- google-remarketing-tags.js
+++ google-remarketing-tags.js
@@ -17,21 +17,21 @@
  dataLayer.push({
     'event': 'fireRemarketingTag',
     'google_tag_params': {
-      'ecomm_prodid': '8756322256745',//in case two or more products, please separeted by comma, example: 756322256745,233322256988
+      'ecomm_prodid': '8756322256745',//in case two or more products, please separeted by comma, example: 756322256745,5823645123658"
       'ecomm_pagetype': 'cart',
-      'ecomm_totalvalue': 756.00 //total value of products
+      'ecomm_totalvalue': 912.00 //total value of products
     }
    });
    
    
-   // Measure view a cart
+// Measure a purchase
 // and a list of productFieldObjects.
  
  dataLayer.push({
     'event': 'fireRemarketingTag',
     'google_tag_params': {
-      'ecomm_prodid': '8756322256745',//in case two or more products, please separeted by comma, example: 756322256745,233322256988
+      'ecomm_prodid': '8756322256745',//in case two or more products, please separeted by comma, example: 756322256745,5823645123658
       'ecomm_pagetype': 'purchase',
-      'ecomm_totalvalue': 756.00 //total value of products
+      'ecomm_totalvalue': 912.00 //total value of products
     }
  });
